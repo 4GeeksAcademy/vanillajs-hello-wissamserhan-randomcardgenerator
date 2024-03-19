88ac752fb4541945da00988af5fb3b961e879194
wissamserhan <116583162+wissamserhan@users.noreply.github.com>
2024-03-19 03:23:19 +0000

    miproyecto

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = onload;
+
+function onload() {
+  const pinta = ["♦", "♥", "♠", "♣"];
+  const numero = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"];
+
+  const aleatoriopinta = Math.floor(Math.random() * pinta.length);
+  const aleatorionumero = Math.floor(Math.random() * numero.length);
+
+  const textNumber = document.createTextNode(numero[aleatorionumero]);
+  const textPintaTop = document.createTextNode(pinta[aleatoriopinta]);
+  const textPintaBotton = document.createTextNode(pinta[aleatoriopinta]);
+
+  const numberNode = document.getElementById("numero");
+  const pintaTopNode = document.getElementById("pintatop");
+  const pintaBottonNode = document.getElementById("pintabotton");
+
+  numberNode.appendChild(textNumber);
+  pintaTopNode.appendChild(textPintaTop);
+  pintaBottonNode.appendChild(textPintaBotton);
+
+  if (aleatoriopinta === 0 || aleatoriopinta === 1) {
+    pintaTopNode.classList.add("red");
+    pintaBottonNode.classList.add("red");
+  }
+}
